6e5b0ce435c6580fb5502a46506effdddad37291
Nobuto Murata <nobuto@ubuntu.com>
2022-06-07 11:20:29 +0900

    patch katib-controller for argo workflow
    trial-resources=Workflow.v1alpha1.argoproj.io

--- redeploy-microk8s-kubeflow.sh
+++ redeploy-microk8s-kubeflow.sh
@@ -22,7 +22,13 @@ juju config dex-auth public-url=http://10.64.140.43.nip.io
 juju config oidc-gatekeeper public-url=http://10.64.140.43.nip.io
 juju config dex-auth static-username=admin
 juju config dex-auth static-password=admin
+
 # https://github.com/canonical/kfp-operators/pull/49
 juju refresh kfp-profile-controller --channel edge
+
+# https://github.com/canonical/bundle-kubeflow/issues/459
+microk8s kubectl patch Deployment katib-controller -n kubeflow --type=json \
+  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--trial-resources=Workflow.v1alpha1.argoproj.io"}]'
+
 time sleep 60
 time microk8s kubectl wait -n kubeflow deployment --all --for condition=Available=True --timeout=1h
