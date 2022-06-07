d4abb18df8a1770f1cd46f3d50b94e82768a0f41
Nobuto Murata <nobuto@ubuntu.com>
2022-06-07 23:11:44 +0900

    Revert "patch katib-controller for argo workflow"
    Remove this workaround for now since this may not be an actual solution
    
    This reverts commit 6e5b0ce435c6580fb5502a46506effdddad37291.

--- redeploy-microk8s-kubeflow.sh
+++ redeploy-microk8s-kubeflow.sh
@@ -28,8 +28,8 @@ juju config dex-auth static-password=admin
 juju refresh kfp-profile-controller --channel edge
 
 # https://github.com/canonical/bundle-kubeflow/issues/459
-microk8s kubectl patch Deployment katib-controller -n kubeflow --type=json \
-  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--trial-resources=Workflow.v1alpha1.argoproj.io"}]'
+#microk8s kubectl patch Deployment katib-controller -n kubeflow --type=json \
+#  -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--trial-resources=Workflow.v1alpha1.argoproj.io"}]'
 
 time sleep 60
 time microk8s kubectl wait -n kubeflow deployment --all --for condition=Available=True --timeout=1h
