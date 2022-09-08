e649fbca95875fc165caae12035bb0ea7029d01e
Nobuto Murata <nobuto@ubuntu.com>
2022-09-08 20:21:29 +0900

    cleanup for testing 1.6

--- redeploy-microk8s-kubeflow.sh
+++ redeploy-microk8s-kubeflow.sh
@@ -6,11 +6,7 @@ set -x
 time sudo snap remove --purge microk8s || true
 juju unregister microk8s-localhost -y || true
 
-# TODO: use 1.21/edge or something once a new revision is published to
-# the snap store:
-# https://github.com/canonical/microk8s/pull/3206
-# https://github.com/canonical/microk8s/issues/3226
-time sudo snap install microk8s --classic --channel 1.21
+time sudo snap install microk8s --classic --channel 1.22
 
 time microk8s status --wait-ready
 time microk8s enable dns storage ingress metallb:10.64.140.43-10.64.140.49
@@ -27,17 +23,8 @@ juju config oidc-gatekeeper public-url=http://10.64.140.43.nip.io
 juju config dex-auth static-username=admin
 juju config dex-auth static-password=admin
 
-time sleep 300
-# juju-wait is not suitable for the time being
-# https://github.com/canonical/bundle-kubeflow/issues/458
-time microk8s kubectl wait -n kubeflow deployment --all --for condition=Available=True --timeout=1h
-
-# https://github.com/canonical/bundle-kubeflow/issues/459
-#microk8s kubectl -n kubeflow rollout restart deployment/katib-controller
-microk8s kubectl -n kubeflow delete pod -l app.kubernetes.io/name=katib-controller
-
-time sleep 120
-time microk8s kubectl wait -n kubeflow deployment --all --for condition=Available=True --timeout=1h
+time juju-wait -w
+#time microk8s kubectl wait -n kubeflow deployment --all --for condition=Available=True --timeout=1h
 
 # TODO: check this just after and 48 hours after the deployment
 curl -svL -o/dev/null http://10.64.140.43.nip.io/
